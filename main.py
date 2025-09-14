e621971373ac179f3de550c5c6d32bac178dba9c
zaptax99 <fischer.p9@gmail.com>
2025-09-14 20:33:11 +0200

    elso modositas

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# This is a sample Python script.
+# Modositas 1.0
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
